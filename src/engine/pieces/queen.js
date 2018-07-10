6c846c5e8200076a5ed412220fe4d0ce34d92658
Jo Lee <jo.lee@softwire.com>
2018-07-10 11:35:16 +0100

    Queen moves laterally, diagonally, and nowhere else

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import GameSettings from "../gameSettings";
+import Square from "../square";
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +8,26 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const moves = [];
+        const pos = board.findPiece(this);
+        for (let i=1; i<GameSettings.BOARD_SIZE; i++) {
+            //lateral moves
+            moves.push(Square.at((pos.row+i)%8, pos.col));
+            moves.push(Square.at(pos.row, (pos.col+i)%8));
+            //diagonal moves
+            if (board.onBoard(Square.at(pos.row+i,pos.col+i))) {
+                moves.push(Square.at(pos.row+i,pos.col+i));
+            }
+            if (board.onBoard(Square.at(pos.row+i,pos.col-i))) {
+                moves.push(Square.at(pos.row+i,pos.col-i));
+            }
+            if (board.onBoard(Square.at(pos.row-i,pos.col+i))) {
+                moves.push(Square.at(pos.row-i,pos.col+i));
+            }
+            if (board.onBoard(Square.at(pos.row-i,pos.col-i))) {
+                moves.push(Square.at(pos.row-i,pos.col-i));
+            }
+        }
+        return moves;
     }
 }
