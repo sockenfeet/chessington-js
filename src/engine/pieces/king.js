719f0209f69ffa4629b707edb0618b7d14119b2f
Jo Lee <jo.lee@softwire.com>
2018-07-10 17:06:24 +0100

    king only takes those he should (what a noble leader!)

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -12,8 +12,11 @@ export default class King extends Piece {
         let pos = board.findPiece(this);
         for (let i=-1; i<=1; i++) {
             for (let j=-1; j<=1; j++) {
-                if (board.onBoard(Square.at(pos.row+i, pos.col+j)) && !(i===0 && j===0)) {
-                    moves.push(Square.at(pos.row+i, pos.col+j));
+                let square = Square.at(pos.row+i, pos.col+j);
+                if (board.onBoard(square) && !(i===0 && j===0)) {
+                    if (board.isFree(square) || board.getPiece(square).isCapturableBy(this.player)) {
+                        moves.push(square);
+                    }
                 }
             }
         }
