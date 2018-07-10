15f82f370f1e428afdaf0673b602d7d9f09f563d
Jo Lee <jo.lee@softwire.com>
2018-07-10 12:02:28 +0100

    The King has been liberated to move as he is able

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,15 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let moves = [];
+        let pos = board.findPiece(this);
+        for (let i=-1; i<=1; i++) {
+            for (let j=-1; j<=1; j++) {
+                if (board.onBoard(Square.at(pos.row+i, pos.col+j)) && !(i===0 && j===0)) {
+                    moves.push(Square.at(pos.row+i, pos.col+j));
+                }
+            }
+        }
+        return moves;
     }
 }
