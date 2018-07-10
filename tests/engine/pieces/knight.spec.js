081ecd03b41876e4569af217bbb492733185261b
Jo Lee <jo.lee@softwire.com>
2018-07-10 16:38:26 +0100

    tests for knight taking

--- tests/engine/pieces/knight.spec.js
+++ tests/engine/pieces/knight.spec.js
@@ -4,6 +4,7 @@ import Pawn from '../../../src/engine/pieces/pawn';
 import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
+import King from "../../../src/engine/pieces/king";
 
 describe('Knight', () => {
 
@@ -56,4 +57,35 @@ describe('Knight', () => {
 
         moves.should.deep.have.members(expectedMoves);
     });
+
+    it('can take opposing non-king pieces', () => {
+        const knight = new Knight(Player.WHITE);
+        const opposingPiece = new Pawn(Player.BLACK);
+        board.setPiece(Square.at(4, 4), knight);
+        board.setPiece(Square.at(5, 6), opposingPiece);
+
+        const moves = knight.getAvailableMoves(board);
+        moves.should.deep.include(Square.at(5, 6));
+    });
+
+    it('cannot take opposing king', () => {
+        const knight = new Knight(Player.WHITE);
+        const opposingKing = new King(Player.BLACK);
+        board.setPiece(Square.at(4, 4), knight);
+        board.setPiece(Square.at(5, 6), opposingKing);
+
+        const moves = knight.getAvailableMoves(board);
+        moves.should.not.deep.include(Square.at(5, 6));
+    });
+
+    it('cannot take friendly pieces', () => {
+        const knight = new Knight(Player.WHITE);
+        const friendlyPiece = new Pawn(Player.WHITE);
+        board.setPiece(Square.at(4, 4), knight);
+        board.setPiece(Square.at(5, 6), friendlyPiece);
+
+        const moves = knight.getAvailableMoves(board);
+        moves.should.not.deep.include(Square.at(5, 6));
+    });
+
 });
