5db9fd152f8f43055a1e12804a691c626da4bec1
Jo Lee <jo.lee@softwire.com>
2018-07-10 11:57:04 +0100

    Knights move as knights do, and that is all.

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,14 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const pos = board.findPiece(this);
+        const knightMoves = [[2,1], [2,-1], [-2,1], [-2,-1], [1,2], [1,-2], [-1,2], [-1,-2]];
+        const moves = [];
+        knightMoves.forEach(move => {
+            if (board.onBoard(Square.at(pos.row + move[0], pos.col+move[1]))) {
+                moves.push(Square.at(pos.row + move[0], pos.col+move[1]))
+            }
+        });
+        return moves;
     }
 }
