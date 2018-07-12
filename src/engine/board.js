683bc752853b411743bec6e0292f2070d558cf12
Tim Colpus <Tim.Colpus@softwire.com>
2018-07-12 09:46:30 +0100

    findKing function

--- src/engine/board.js
+++ src/engine/board.js
@@ -2,6 +2,7 @@ import Player from './player';
 import GameSettings from './gameSettings';
 import Square from './square';
 import Pawn from './pieces/pawn';
+import King from './pieces/king';
 
 export default class Board {
     constructor(currentPlayer) {
@@ -57,4 +58,36 @@ export default class Board {
     onBoard(square) {
         return 0<=square.row && square.row<GameSettings.BOARD_SIZE && 0<=square.col && square.col<GameSettings.BOARD_SIZE;
     }
+
+    // inspect if board is in state of check for given player
+    inCheck(player) {
+        let kings = this.findKings();
+
+    }
+
+    // find the kings
+    findKings() {
+        let kingW;
+        let kingB;
+        for(let i=0; i<GameSettings.BOARD_SIZE; i++) {
+            for (let j = 0; j < GameSettings.BOARD_SIZE; j++) {
+                if (this.getPiece(Square.at(i, j)) instanceof King) {
+                    switch (this.getPiece(Square.at(i, j)).player) {
+                        case Player.WHITE:
+                            kingW = Square.at(i, j);
+                            break;
+                        case Player.BLACK:
+                            kingB = Square.at(i, j);
+                            break;
+                    }
+                }
+            }
+        }
+        return [kingW, kingB];
+    }
+
+}
+}
+}
+}
 }
